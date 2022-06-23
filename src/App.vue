49233a4cf34776f8499c3bf1b8682b7a7ac482df
Simone Carannante <simonecarannante@outlook.it>
2022-06-23 20:19:09 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <h1 class="text-center">Il nostro primo esercizio in VUE</h1>
+  <div class="row">
+    <div v-for="announcement in announcements" class="col-12 col-md-4">
+        <h2 class="text-center">{{announcement.name}}</h2>
+        <button class="btn btn-outline-info d-block mx-auto">{{announcement.category}}</button>
+        <h3 class="text-center">{{announcement.price}}&euro;</h3>
+        <button @click="salvaProdotto(announcement)" class="btn btn-info d-block mx-auto" data-bs-toggle="modal" data-bs-target="#exampleModal">Dettagli</button>
+
+
+    
+    </div>
+  </div>
+</div>
+
+<Detail :announcement="announcement" />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Detail from './components/Detail.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{Detail},  // E' un set, ovvero non accetta duplicazioni
+
+  data(){
+    return {
+      announcements: [
+        {id:1, name: 'Armadio', category: 'Arredamento', price:2000},
+        {id:2, name: 'Quadro', category: 'Arte', price:1000},
+        {id:3, name: 'Automobile', category: 'Motori', price:200000}
+
+        ],
+        announcement : {}
+    }
+  },
+  methods:{
+    salvaProdotto(prodotto){
+      this.announcement = prodotto
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Detail.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">{{announcement.name}}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        {{announcement.category}}
+        {{announcement.price}}
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+    export default{
+        name: 'Detail',
+        props: ['announcement'],
+    }
+
+</script>
+
+<style>
+
+</style>
